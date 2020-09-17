971f2474b30901895c4590fc5310341d93b5baab
ForestSquirrel <Vasiliy.14vasiliy@yandex.ru>
2020-09-18 02:34:16 +0300

    damn token

--- Source.cpp
+++ Source.cpp
@@ -1,16 +1,16 @@
 #include "sleepy_discord/sleepy_discord.h"
 #include "sleepy_discord/websocketpp_websocket.h"
 
-class myClientClass : public SleepyDiscord::DiscordClient {
+class DSClient : public SleepyDiscord::DiscordClient {
 public:
     using SleepyDiscord::DiscordClient::DiscordClient;
     void onMessage(SleepyDiscord::Message message) {
-        if (message.startsWith("whcg hello"))
+        if (message.startsWith("hello"))
             sendMessage(message.channelID, "Hello " + message.author.username);
     }
 };
 
 int main() {
-    myClientClass client("NzQzNDkyMTQ3ODg5ODMxOTg4.XzVdBQ.XE2_HcSqYcOyvfdIygJtnJvg30E", 2);
+    DSClient client("very secret info (token)", 2);
     client.run();
 }
