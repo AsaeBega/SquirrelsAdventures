dff9a000241c243db9ef4f989c9fc55b82b4068a
ForestSquirrel <Vasiliy.14vasiliy@yandex.ru>
2020-09-29 00:12:39 +0300

    finally token is hidden

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,15 @@
 #include "DISCLIENT.h"
+#include <fstream>
+#include <string>
+#include<iostream>
 
 int main() {
-    DSClient client("NzQzNDkyMTQ3ODg5ODMxOTg4.XzVdBQ.Z7dqRuadXRqWR7FQkTQCAAZVfF4", 2);
+    std::ifstream token("C:\\Users\\Squirrel\\source\\repos\\SquirrelsAdventures\\SquirrelsAdventures\\token.txt");
+    std::string token_str;
+    if(token.is_open())
+        std::getline(token, token_str);
+    DSClient client(token_str, 2);
+    //token_str.clear();
+    token.close();
     client.run();
 }
